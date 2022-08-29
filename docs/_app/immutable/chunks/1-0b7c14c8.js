9f70c17c434cd824bad92f6928de36d97a6f4bbf
jeffa-bob <65685968+jeffa-bob@users.noreply.github.com>
2022-08-28 20:08:20 -0700

    add docs

---
+++ docs/_app/immutable/chunks/1-0b7c14c8.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-c014fe21.js";import"./index-2b05990c.js";import"./singletons-b81b494a.js";export{r as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-
+const dev = "production" == "development"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,16 +9,15 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		prerender: {default:true},
 		adapter: adapter({
             pages: "docs",
             assets: "docs"
         }),
         paths: {
             // change below to your repo name
-            base: dev ? "" : "/your-repo-name",
+            base: dev ? "" : "/Donation-Tracker",
         },
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: "#svelte"
 	}
 };
 
